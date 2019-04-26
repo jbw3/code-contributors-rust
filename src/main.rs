6de4aa85cdef4a701af9f7b1e729172ad3d66b0c
John Wilkes <jwilkes0011@gmail.com>
2019-04-25 21:43:50 -0500

    Finding commiter name

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use regex::Regex;
 use std::io::{self, BufRead};
 
 fn main()
 {
+    let re = Regex::new(r"\((\S*(?: +\S+)*) +\d{4}").unwrap();
     let stdin = io::stdin();
     for line in stdin.lock().lines()
     {
-        println!("{}", line.unwrap());
+        let s = line.unwrap();
+        let cap = re.captures(&s).unwrap();
+
+        println!("'{}'", &cap[1]);
     }
 }
