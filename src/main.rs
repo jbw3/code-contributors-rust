081224fcd5b0ee14206478b5f19fd1a729bc971f
John Wilkes <jwilkes0011@gmail.com>
2019-04-25 22:29:12 -0500

    Counting lines

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 use regex::Regex;
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 fn main()
 {
     let re = Regex::new(r"\((\S*(?: +\S+)*) +\d{4}").unwrap();
+    let mut line_counts: HashMap<String, u32> = HashMap::new();
+
+    // loop through stdin lines
     let stdin = io::stdin();
     for line in stdin.lock().lines()
     {
+        // get name
         let s = line.unwrap();
         let cap = re.captures(&s).unwrap();
+        let name: String = cap[1].to_string();
 
-        println!("'{}'", &cap[1]);
+        // increment count
+        if let Some(count) = line_counts.get_mut(&name)
+        {
+            *count += 1;
+        }
+        else
+        {
+            line_counts.insert(name, 1);
+        }
+    }
+
+    for (key, val) in line_counts.iter()
+    {
+        println!("{}: {}", key, val);
     }
 }
